397450cc96cbf6290028a5562e98e25dba627205
Dilkhush gurjar <106782842+Dk143gurjar@users.noreply.github.com>
2022-11-08 11:23:36 +0530

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by Dkmusic@Github, < https://github.com/Dk143gurjar >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/Dk143gurjar/Dilkhush > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/Dk143gurjar/Dilkhush/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -15,13 +15,13 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS
 from strings import get_command, get_string, helpers
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils import help_pannel
-from YukkiMusic.utils.database import get_lang, is_commanddelete_on
-from YukkiMusic.utils.decorators.language import (LanguageStart,
+from DkMusic import app
+from DkMusic.misc import SUDOERS
+from DkMusic.utils import help_pannel
+from DkMusic.utils.database import get_lang, is_commanddelete_on
+from DkMusic.utils.decorators.language import (LanguageStart,
                                                   languageCB)
-from YukkiMusic.utils.inline.help import (help_back_markup,
+from DkMusic.utils.inline.help import (help_back_markup,
                                           private_help_panel)
 
 ### Command
